2d6384fd088f026868b8951e50545b5e95bace71
denis <dens.andreev@cti.ru>
2021-05-14 18:58:20 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import csv
+
+def inflation_view(request):
+    context = {
+        'header': [],
+        'data': []
+    }
+    template_name = 'inflation.html'
+
+    with open('inflation_russia.csv', newline='', encoding='utf-8') as f:
+        file_data = csv.reader(f, delimiter=';')
+        for i, row in enumerate(file_data):
+            if i == 0:
+                context['header'] = row
+            else:
+                context['data'].append(row)
+        print(context)
+    return render(request, template_name, context)
